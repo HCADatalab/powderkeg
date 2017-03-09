cefffa13e55bf71b1208b4fd3d875b784bca50c9
Christophe Grand <christophe@cgrand.net>
2017-03-09 13:24:50 +0100

    No need for explicit chill

--- project.clj
+++ project.clj
@@ -6,14 +6,10 @@
   
   :profiles {:spark2 [^{:pom-scope :provided}
                       {:dependencies [[org.apache.spark/spark-core_2.11 "2.1.0"]
-                                      [org.apache.spark/spark-streaming_2.11 "2.1.0"]]}
-                      {:dependendies [[com.twitter/chill_2.11 "0.5.0" ; for carbonite
-                                       :exclusions [org.scala-lang/scala-library]]]}]
+                                      [org.apache.spark/spark-streaming_2.11 "2.1.0"]]}]
              :spark1.5 [^{:pom-scope :provided}
                         {:dependencies [[org.apache.spark/spark-core_2.10 "1.5.2"]
-                                        [org.apache.spark/spark-streaming_2.10 "1.5.2"]]}
-                        {:dependendies [[com.twitter/chill_2.10 "0.5.0" ; for carbonite
-                                         :exclusions [org.scala-lang/scala-library]]]}]}
+                                        [org.apache.spark/spark-streaming_2.10 "1.5.2"]]}]}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  
                  [net.cgrand/xforms "0.5.1"]
