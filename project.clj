905babf0e0c3ed828771b9786fc530a0d49a15c5
Kimmo Koskinen <kimmo@tiuhti.net>
2017-03-14 20:55:49 +0200

    Use kryo 3.0.3 for spark 1.5, kryo 4.0.0 for spark 2

--- project.clj
+++ project.clj
@@ -7,17 +7,18 @@
   :test-selectors {:default (complement :integration)
                    :integration :integration}
   :profiles {:default [:spark2]
-             :spark2 [:leiningen/default :spark2-deps]
-             :spark1.5 [:leiningen/default :spark1.5-deps]
+             :spark2 [:leiningen/default :spark2-deps :kryo-4]
+             :spark1.5 [:leiningen/default :spark1.5-deps :kryo-3]
              :spark2-deps ^{:pom-scope :provided}
              {:dependencies [[org.apache.spark/spark-core_2.11 "2.1.0"]
                              [org.apache.spark/spark-streaming_2.11 "2.1.0"]]}
              :spark1.5-deps ^{:pom-scope :provided}
              {:dependencies [[org.apache.spark/spark-core_2.10 "1.5.2"]
-                             [org.apache.spark/spark-streaming_2.10 "1.5.2"]]}}
+                             [org.apache.spark/spark-streaming_2.10 "1.5.2"]]}
+             :kryo-3 {:dependencies [[com.esotericsoftware/kryo-shaded "3.0.3"]]}
+             :kryo-4 {:dependencies [[com.esotericsoftware/kryo-shaded "4.0.0"]]}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.cgrand/xforms "0.5.1"]
-                 [com.esotericsoftware/kryo-shaded "4.0.0"]
                  [com.twitter/carbonite "1.4.0" :exclusions [com.twitter/chill-java]]
                  [com.cemerick/pomegranate "0.3.0"]]
   :aot [powderkeg.repl]
