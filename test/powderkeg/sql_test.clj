5953710645dd572e6dbb6955e6cc62a0dea40bb6
Kimmo Koskinen <kimmo@tiuhti.net>
2017-03-20 11:46:00 +0200

    Add assert for sql/spec-of

--- test/powderkeg/sql_test.clj
+++ test/powderkeg/sql_test.clj
@@ -12,4 +12,10 @@
     [local-spark]
     (let [data-set (sql/df [{::name "Brian"}] ::person)]
       (is (= "Brian"
-             (.getString (.first data-set) 0))))))
+             (.getString (.first data-set) 0)))
+      (is (= (s/form ::person)
+             (s/form (sql/spec-of data-set))))
+      (.createTempView data-set "people")
+      (let [selection (sql/exec "select * from people")]
+        (is (= (s/form ::person)
+               (s/form (sql/spec-of selection))))))))
