41d403ef71d9098055802abde79ad606e4959940
Kimmo Koskinen <kimmo@tiuhti.net>
2017-03-22 18:26:19 +0200

    Add tests for sql namespace (df, spec-of and exec)

---
+++ test/powderkeg/sql_test.clj
@@ -0,0 +1,22 @@
+(ns powderkeg.sql-test
+  (:require [powderkeg.sql :as sql]
+            [clojure.test :refer :all]
+            [powderkeg.fixtures :refer [with-resources local-spark]]
+            [clojure.spec :as s]))
+
+(s/def ::name string?)
+(s/def ::person (s/keys :req [::name]))
+
+(deftest sql
+  (with-resources
+    [local-spark]
+    (let [data-set (sql/df [{::name "Brian"} {::name "Brita"}] ::person)]
+      (is (= ["Brian" "Brita"]
+             (map #(.getString % 0) (.collect data-set))))
+      (is (= (s/form ::person)
+             (s/form (sql/spec-of data-set))))
+      (when (.startsWith (.version powderkeg.core/*sc*) "2.")
+        (.createTempView data-set "people")
+        (let [selection (sql/exec "select * from people")]
+          (is (= (s/form ::person)
+                 (s/form (sql/spec-of selection)))))))))
