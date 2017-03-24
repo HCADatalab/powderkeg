4ea9331b7c03dba7424c348e97d7d2d679f6abe1
Kimmo Koskinen <kimmo@tiuhti.net>
2017-03-24 16:30:12 +0200

    Collect to map in order to not rely on ordering

--- test/powderkeg/core_test.clj
+++ test/powderkeg/core_test.clj
@@ -41,8 +41,8 @@
          (keg/into [] (partition-by #(quot % 6)) (keg/rdd (range 20)))))
   (is (= [[0 1 2 3 4 5] [6 7 8 9 10 11] [12 13 14 15 16 17] [18 19]]
          (into [] (keg/scomp (partition-by #(quot % 6))) (keg/rdd (range 20)))))
-  (is (= [[:a [[1 11] "aa"]] [:c ["x" "cc"]] [:b [2 "y"]]]
-         (into [] (keg/join (keg/rdd {:a [1 11] :b 2}) :or "x" (keg/rdd {:a "aa" :c "cc"} ) :or "y")))))
+  (is (= {:a [[1 11] "aa"] :c ["x" "cc"] :b [2 "y"]}
+         (into {} (keg/join (keg/rdd {:a [1 11] :b 2}) :or "x" (keg/rdd {:a "aa" :c "cc"} ) :or "y")))))
 
 
 (deftest pair-rdd
