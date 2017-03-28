fff8dc28b874c8bedd3a6902c860724a0ba5fcd9
Christophe Grand <christophe@cgrand.net>
2017-03-28 10:45:34 +0200

    failting test for #27

--- test/powderkeg/core_test.clj
+++ test/powderkeg/core_test.clj
@@ -40,3 +40,9 @@
 (deftest pair-rdd
   (is (instance? org.apache.spark.api.java.JavaRDD
                  (keg/rdd {:a 1 :b 2}))))
+
+(deftest redef
+  (is (= (eval '(do (defn ++ [x] (inc x)) (into [] (powderkeg.core/rdd (range 5) (map ++)))))
+        (map inc (range 5))))
+  (is (= (eval '(do (defn ++ [x] (dec x)) (into [] (powderkeg.core/rdd (range 5) (map ++)))))
+        (map dec (range 5)))))
